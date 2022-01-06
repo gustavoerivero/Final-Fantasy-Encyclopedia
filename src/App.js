deaf0607eb4d03f89ff08a8e4d042ceff09d97a8
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-01-06 14:21:23 -0430

    Update

--- src/App.js
+++ src/App.js
@@ -19,10 +19,9 @@ export default function App() {
           <Router>
             <CssBaseline />
             <AppNavigator />
-            <Route exact path='/' component={Home} />
+            <Route exact path='/Final-Fantasy-Encyclopedia' component={Home} />
             <Route exact path='/character/:name' component={CharacterDetails} />
             <Route exact path='/favorites' component={Favorites} />
-            <Redirect from='*' to='/' />
             <Footer />
           </Router>
         </ThemeProvider>
