cd5fa54246bdb86aae1d6aa753c08b9957c38992
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-01-06 14:45:39 -0430

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
 import Theme from './static/theme/Theme'
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { Provider } from 'react-redux'
